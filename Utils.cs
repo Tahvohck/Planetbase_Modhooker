cc914767f4a52cbd7e8fb36bccc2aa54d5509915
Tahvohck <Tahvohck@gmail.com>
2020-05-29 04:00:27 -0400

    Added enums for use with the maniac flags Madruga included

--- Utils.cs
+++ Utils.cs
@@ -50,20 +50,72 @@ namespace Tahvohck_Mods
 
 
     /// <summary>
-    /// Best guesses when trawling through code.
+    /// Component flags based on the huge amount of ints in <see cref="Planetbase.ComponentType"/>
     /// </summary>
     public enum ComponentFlags
     {
-        HumanInBed =    0x000_0002,     // Combined with 0x40 for normal bed
-        Automatic =     0x000_0008,     // In code as this OR base.anyInteractions()
-        CanDisable =    0x000_0020,
-        InBed =         0x000_0040,     // In sick bay code
-        FastRate =      0x000_0800,
-        SlowRate =      0x000_0400,
-        NeedsDay =      0x000_8000,
-        AltProduce =    0x002_0000,     // in GuiInfoPanel
-        Consumes =      0x004_0000,     // Or Produces, or getNeededResources?
-        HasAnchor =     0x040_0000,
-        DisasterWatch = 0x800_0000
+        Extractor =         0x000_0001,
+        Sleeper =           0x000_0002, // Combined with 0x40 for normal bed
+        NeedsRepair =       0x000_0004,
+        NeedsOperator =     0x000_0008, // In code as this OR base.anyInteractions()
+
+        Relaxing =          0x000_0010,
+        CanDisable =        0x000_0020,
+        Healing =           0x000_0040, // In sick bay code
+        NoPowerNeed =       0x000_0080,
+
+        WallPart =          0x000_0100,
+        SlowRate =          0x000_0400,
+        FastRate =          0x000_0800,
+
+        Hydrating =         0x000_1000,
+        // There's seriously a gap here for no good reasons.
+        IsTree =            0x000_8000,
+
+        IsHanging =         0x001_0000,
+        AltProduce =        0x002_0000, // in GuiInfoPanel
+        AltConsume =        0x004_0000, // Also in GuiInfoPanel
+        AwayFromCorridor =  0x008_0000,
+
+        StoreProduct =      0x010_0000,
+        QuadrantAutoRot =   0x020_0000, // What the fuck
+        AutoAnchor =        0x040_0000,
+        HasSparks =         0x080_0000,
+
+        FaceCorridor =      0x100_0000,
+        NeedsTracksuit =    0x200_0000,
+        UncoveredHead =     0x400_0000,
+        DisasterDetector =  0x800_0000
+    }
+
+
+    /// <summary>
+    /// Based on the ints in <see cref="Planetbase.ModuleType"/>
+    /// </summary>
+    public enum ModuleFlags
+    {
+        LandingPad =        0x00_0001,
+        Mine =              0x00_0002,
+        Airlock =           0x00_0004,
+        Storage =           0x00_0008,
+        Dome =              0x00_0010,
+        LightAtNight =      0x00_0020,
+        NeedsWind =         0x00_0040,
+        NeedsLight =        0x00_0080,
+        NoFoundation =      0x00_0100,
+        DeadEnd =           0x00_0400,
+        Walkable =          0x00_0800,
+        Blinkenlights =     0x00_1000,
+        SnapComponent =     0x00_2000,
+        Starport =          0x00_4000,
+        Autorotate =        0x00_8000,
+        Animated =          0x01_0000,
+        CylinderBase =      0x02_0000,
+        RemoteOperate =     0x04_0000,
+        ScanAnimation =     0x08_0000,
+        Prioritizable =     0x10_0000,
+        AntiMeteor =        0x20_0000,
+        LightningRod =      0x40_0000,
+        DisasterDetector =  0x80_0000
     }
 }
