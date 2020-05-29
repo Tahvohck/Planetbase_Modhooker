4ec3fa60ae253954e2f1d1a5067eeb9e90234ef1
Tahvohck <Tahvohck@gmail.com>
2020-05-29 02:11:41 -0400

    Inital working version

---
+++ Utils.cs
@@ -0,0 +1,69 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Tahvohck_Mods
+{
+    public class TahvUtil
+    {
+        /// <summary>
+        /// Logfile gathered from Planetbase's utils since it's there.
+        /// </summary>
+        public readonly static string Logfile = Util.getFilesFolder() + "/Modhooker.log";
+
+        /// <summary>
+        /// Log to both the unity debug log and modhooker.log.
+        /// </summary>
+        /// <param name="message">Object to log to file. Will be converted to a string via ToString</param>
+        /// <param name="context">Unity Context. Largely unneeded, but might be useful if you're using
+        /// a debugger.</param>
+        public static void Log(object message, UnityEngine.Object context = null)
+        {
+            string logMessage;
+            MethodBase method = new StackFrame(1).GetMethod();
+            logMessage = $"[{method.DeclaringType.FullName}] {message}";
+            
+            // Log to Logfile, if errors are encountered then modify the logMessage.
+            try {
+                File.AppendAllText(Logfile, logMessage + Environment.NewLine, Encoding.ASCII);
+            } catch (Exception ex) {
+                logMessage = $"{logMessage}" +
+                    $"\n  Error logging this message to the modhooker log: {Logfile}" +
+                    $"\n  {ex.Message}";
+            }
+            UnityEngine.Debug.Log(logMessage, context);
+        }
+
+        /// <summary>
+        /// Only meant to be used internally to clear the log before use.
+        /// </summary>
+        internal static void ClearLog()
+        {
+            File.WriteAllText(Logfile, string.Empty);
+        }
+    }
+
+
+    /// <summary>
+    /// Best guesses when trawling through code.
+    /// </summary>
+    public enum ComponentFlags
+    {
+        HumanInBed =    0x000_0002,     // Combined with 0x40 for normal bed
+        Automatic =     0x000_0008,     // In code as this OR base.anyInteractions()
+        CanDisable =    0x000_0020,
+        InBed =         0x000_0040,     // In sick bay code
+        FastRate =      0x000_0800,
+        SlowRate =      0x000_0400,
+        NeedsDay =      0x000_8000,
+        AltProduce =    0x002_0000,     // in GuiInfoPanel
+        Consumes =      0x004_0000,     // Or Produces, or getNeededResources?
+        HasAnchor =     0x040_0000,
+        DisasterWatch = 0x800_0000
+    }
+}
---
+++ ZZZ_Modhooker_Main.cs
@@ -0,0 +1,77 @@
+﻿using HarmonyLib;
+using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Tahvohck_Mods
+{
+    public class ZZZ_Modhooker : IMod
+    {
+        public void Init()
+        {
+            FileLog.logPath = "./harmony.log";
+            TahvUtil.ClearLog();
+            string ver = FileVersionInfo
+                .GetVersionInfo(Assembly.GetExecutingAssembly().Location)
+                .FileVersion;
+            TahvUtil.Log($"Initialized Modhooker v{ver}");
+
+            // Call all code that needs to have the mInstance lists reset after it runs.
+            OnPreResetEventHandler(EventArgs.Empty);
+
+            // By setting these to null, the game will natively restore them during getInstance()
+            PlanetList.mInstance = null;
+            ModuleTypeList.mInstance = null;
+            NameGenerator.mInstance = null; // No issues found. Names generate fine.
+            CharacterDefinitions.mInstance = null;  // No issues found. Characters seem fine.
+            ExtraQualitySettings.mInstance = null;  // Possible use: More quality settings?
+
+
+            // Dangerous mInstances that fuck the game up.
+            // ResourceList: Buggy in code. Does not check for null.
+            // TODO: Harmony patch to un-fuck this?
+            //ResourceList.mInstance = new ResourceList();
+            // Fucks the planet selection screen for... some reason... It only gets referenced by
+            // getInstance() for reads, but by .cctor(), CameraManager()
+            // TODO: Find out why???
+            //CameraManager.mInstance = null;
+            // Issues with these enabled in the game. Will not enable until I have a reason.
+            //MixerGroups.mInstance = null;     // Audio mixer. Nothing to Harmonize.
+            //ProfileManager.mInstance = null;  // Seems to be some sort of debugger
+            //SoundList.mInstance = null;       // No good reason to overload
+            //GameManager.mInstance = null;     // A lot of shit in here. See what it does. Looks powerful.
+
+            TahvUtil.Log("Done resetting lists.");
+            OnPostResetEventHandler(EventArgs.Empty);
+        }
+
+        public void Update()
+        {
+            // Don't need to do anything here
+            return;
+        }
+
+        public static event EventHandler PreResetEvent;
+        public static event EventHandler PostResetEvent;
+
+        protected virtual void OnPreResetEventHandler(EventArgs e)
+        {
+            EventHandler handler = PreResetEvent;
+            if (!(handler is null)) {
+                handler.Invoke(this, e);
+            }
+        }
+
+        protected virtual void OnPostResetEventHandler(EventArgs e)
+        {
+            EventHandler handler = PostResetEvent;
+            if (!(handler is null)) {
+                handler.Invoke(this, e);
+            }
+        }
+    }
+}
